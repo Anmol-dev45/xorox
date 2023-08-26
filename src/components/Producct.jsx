dba53cd64bcd4d12d8585cae6efa225d8f99ea01
shreezan21 <126370801+shreezan21@users.noreply.github.com>
2023-08-26 16:43:12 +0545

    a

--- src/components/Producct.jsx
+++ src/components/Producct.jsx
@@ -1,38 +1,33 @@
-import React from 'react';
-import { PRODUCT } from '../constant';
+import React from 'react'
+import { PRODUCT } from '../constant'
+const Producct = () => {
+    return (
+        <section id='product' className='py-20 md:py-28'>
+            <div className='container mx-auto px-4'>
+                <h2 className='capitalize font-ubuntu text-6xl font-bold text-dark text-center'>our products</h2>
+                
+                <div className='flex justify-center gap-16 mt-10 flex-wrap '>
+                    {
+                        PRODUCT && (
+                            PRODUCT.map(item => (
+                                <div key={item.name} className='flex flex-col even:md:flex-row  odd:md:flex-row-reverse items-start justify-center md:items-center gap-y-[80px] ' >
+                                    <div className='flex-1'>
+                                        <img src={item.path} alt={`image of ${item.name}`} className='max-w-[30%] mx-auto' />
+                                    </div>
+                                    <div className='flex-1'>
 
-const Product = () => {
-  return (
-    <section id='product' className='py-20 md:py-28'>
-      <div className='container mx-auto px-4'>
-        <h2 className='capitalize font-ubuntu text-6xl font-bold text-dark text-center'>our products</h2>
-
-        <div className='flex justify-center gap-16 mt-10 flex-wrap'>
-          {PRODUCT && (
-            PRODUCT.map(item => (
-              <div key={item.name} className='flex flex-col even:md:flex-row odd:md:flex-row-reverse items-start justify-center md:items-center gap-y-[80px]'>
-                <div className='flex-1'>
-                  {/* Background Image */}
-                  <div className='relative flex-1 flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12'>
-                    <img src="/img/beams.jpg" alt="" className='absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2' width="1308" />
-                    {/* Product Image */}
-                    <div className='flex-1'>
-                      <img src={item.path} alt={`image of ${item.name}`} className='max-w-[30%] mx-auto' />
-                    </div>
-                  </div>
+                                        <h3 className='text-[4rem] tracking- font-semibold font-mukta mt-10 mb-4 text-left tracking-wider text-primary'>{item.name}</h3>
+                                        <p className='text-justify leading-[1.3rem] text-lg font-mukta text-black-600  tracking-wide mb-10 md:pr-32'>{item.description}</p>
+                                        <a href="" className='text-blue-500 font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing.</a>
+                                    </div>
+                                </div>
+                            ))
+                        )
+                    }
                 </div>
-                <div className='flex-1'>
-                  <h3 className='text-[4.5rem] tracking- font-semibold font-mukta mt-10 mb-4 text-left tracking-wider text-primary'>{item.name}</h3>
-                  <p className='text-justify leading-[1.3rem] text-lg font-mukta text-black-600  tracking-wide mb-10 md:pr-32'>{item.description}</p>
-                  <a href="" className='text-blue-500 font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing.</a>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-      </div>
-    </section>
-  );
+            </div>
+        </section>
+    )
 }
 
-export default Product;
+export default Producct
