32739026dcb6fa64f3846ea9869ba135501eb9be
Anmol-dev45 <basnetanmol2020@gmail.com>
2023-09-04 22:06:50 +0545

    Removed unwanted data

--- src/constant.js
+++ src/constant.js
@@ -4,11 +4,7 @@ export const PRODUCT = [
         name: "VoyageGuard",
         description: "VoyageGuard is a reliable platform that offers tourists a safe and secure way to explore Nepal. Here, you can access various services such as assistance with taxi fares, destination guides, and trusted hotels at fair prices. We strive to provide comprehensive support in all aspects of your travel experience. To access these services, simply click the button below and start your journey with confidence.",
         path: "/images/voyagegard.png"
-    }, {
-        name: "Xordrobe",
-        description: "Xordrobe is an innovative ecommerce platform that offers a diverse range of clothing items, providing customers with a convenient and enjoyable online shopping experience to explore and purchase the latest fashion trends.",
-        path: "/images/aa.png"
-    }
+    }, 
 
 ]
 
