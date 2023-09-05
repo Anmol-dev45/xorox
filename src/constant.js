8878cbba2f6a7610959021bb9270c13b678cadd6
shreezan21 <126370801+shreezan21@users.noreply.github.com>
2023-09-05 18:22:12 +0545

    photo change

--- src/constant.js
+++ src/constant.js
@@ -12,19 +12,19 @@ export const TEAM = [
     {
         name: "Srijan Subedi",
         position: "CEO and Founder",
-        path: "/images/ceo.jpg"
+        path: "/images/srijan.jpg"
     },{
         name: "Anmol Basnet",
         position: "CTO and Co-founder",
-        path: "/images/cto.jpg"
+        path: "/images/anmol.jpg"
     },{
         name: "Riwaz Aryal",
         position: "COO and Co-founder",
-        path: "/images/coo.jpg"
+        path: "/images/riwaj.jpg"
     },{
         name: "Sanjeev Shrestha",
         position: "CMO and Co-founder",
-        path: "/images/cmo.png"
+        path: "/images/sanjeev.png"
     },{
         name: "Sayuj Kuickel",
         position: "Chief Information Officer",
